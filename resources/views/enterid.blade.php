d07ef2e2d3c9816b06814cf083dcb4dee337b6d7
Navaneshwar <navane.mula@gmail.com>
2020-09-20 15:39:58 +0530

    center

--- resources/views/enterid.blade.php
+++ resources/views/enterid.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 @section('content')
+<div class="container text-center">
 {{ Form::open(array('action' => 'PagesController@test','id'=>'addform','method'=> 'get')) }}
     <input type="text" name="id">
     <input type=submit>
     {{ Form::close() }}
+    <div>
 @endsection
